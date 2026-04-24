49262261dd8329907a6ec4ccd04d9407544bd141
DevdiniSenevirathna <devdinichaniya2001@gmail.com>
2026-04-24 18:23:55 +0530

    fix: Conditionally render company and job title

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -8,12 +8,15 @@ const ResumeCard = ({resume: {id, jobTitle, companyName, feedback , imagePath}}:
     return (
         <Link to={`/resume/${id}`} className="resume-card animate-in fade-in duration-1000">
             <div className="resume-card-header">
-                <h2 className="text-black! font-bold wrap-break-word">
-                    {companyName}
-                </h2>
-                <h3 className="text-lg wrap-break-word text-gray-500">
-                    {jobTitle}
-                </h3>
+                <div className="flex flex-col gap-2">
+                    {companyName && <h2 className="text-black! font-bold wrap-break-word">
+                        {companyName}
+                    </h2>}
+                    {jobTitle && <h3 className="text-lg wrap-break-word text-gray-500">
+                        {jobTitle}
+                    </h3>}
+                    {!companyName && !jobTitle && <h2 className="text-black! font-bold">Resume</h2>}
+                </div>
                 <div className="shrink-0">
                     <ScoreCircle score={feedback.overallScore}/>
                 </div>
