30ef01f6a26ea58f67015cec584da001086f714f
DevdiniSenevirathna <devdinichaniya2001@gmail.com>
2026-04-24 18:38:19 +0530

    Remove chrome-devtools route; disable SSR

--- app/routes.ts
+++ app/routes.ts
@@ -3,7 +3,6 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
   index("routes/home.tsx"),
   route("/auth", "routes/auth.tsx"),
-  route("/.well-known/appspecific/com.chrome.devtools.json", "routes/chrome-devtools.ts"),
   route('/upload' , 'routes/upload.tsx'),
   route('/resume/:id' , 'routes/resume.tsx'),
   route('/wipe', 'routes/wipe.tsx'),
