5e57f3e183e3c60ae6abdb8dd0a216278b62f0aa
DevdiniSenevirathna <devdinichaniya2001@gmail.com>
2026-04-24 17:10:01 +0530

    fix: update resume.tsx

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -50,7 +50,7 @@ const Resume = () => {
     }, [id]);
 
     return (
-        <main className="!pt-0">
+        <main className="pt-0!">
             <nav className="resume-nav">
                 <Link to="/" className="back-button">
                     <img src="/icons/back.svg" alt="logo" className="w-2.5 h-2.5" />
@@ -58,12 +58,13 @@ const Resume = () => {
                 </Link>
             </nav>
             <div className="flex flex-row w-full max-lg:flex-col-reverse">
-                <section className="feedback-section bg-[url('/images/bg-small.svg') bg-cover h-[100vh] sticky top-0 items-center justify-center">
+                <section className="feedback-section bg-[url('/images/bg-small.svg') bg-cover h-screen sticky top-0 items-center justify-center">
                     {imageUrl && resumeUrl && (
                         <div className="animate-in fade-in duration-1000 gradient-border max-sm:m-0 h-[90%] max-wxl:h-fit w-fit">
                             <a href={resumeUrl} target="_blank" rel="noopener noreferrer">
                                 <img
                                     src={imageUrl}
+                                    alt="image"
                                     className="w-full h-full object-contain rounded-2xl"
                                     title="resume"
                                 />
@@ -72,7 +73,7 @@ const Resume = () => {
                     )}
                 </section>
                 <section className="feedback-section">
-                    <h2 className="text-4xl !text-black font-bold">Resume Review</h2>
+                    <h2 className="text-4xl text-black! font-bold">Resume Review</h2>
                     {feedback ? (
                         <div className="flex flex-col gap-8 animate-in fade-in duration-1000">
                             <Summary feedback={feedback} />
@@ -80,7 +81,7 @@ const Resume = () => {
                             <Details feedback={feedback} />
                         </div>
                     ) : (
-                        <img src="/images/resume-scan-2.gif" className="w-full" />
+                        <img src="/images/resume-scan-2.gif" alt="image" className="w-full" />
                     )}
                 </section>
             </div>
